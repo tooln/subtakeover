de548fcb2ff6d7063011ac1801f1623cfe315398
Mangekyou Sharingan <110949311+tooln@users.noreply.github.com>
2024-02-21 07:10:36 +0600

    Update r.sh

--- r.sh
+++ r.sh
@@ -11,6 +11,11 @@ git clone https://github.com/PentestPad/subzy.git
 sleep 1
 cd subzy && wget https://tooln.github.io/subtakeover/script.sh && chmod +x *
 sleep 1
+go version && ls
+sleep 1
 go run main.go version
 sleep 1
-figlet "Installation Complete..."
+cd runner && cp fingerprints.json /sec/root/subzy
+sleep 1
+cd /sec/root/subzy && go run main.go run --target evil.com
+sleep 1
